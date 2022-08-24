390809ee32410bc3e9bd22c1e103556f50474ad2
EricZhenPeng <48643613+EricZhenPeng@users.noreply.github.com>
2022-08-24 23:20:54 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="flex justify-content items-center">
+      <div className="bg-orange-200 max-w-3xl">
+        asdasdasd=================dsdsdsd
+      </div>
+      <div>asdasdasd</div>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1 className="text-3xl font-bold underline">Learn React</h1>
+        <div className=""></div>
+        <h1 className="text-3xl font-bold underline">Learn React</h1>
+        <h1 className="">asdasda</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
